d7c20395b95b3d18c3ce69cc45e4a4ee655ce8a9
Oğuzhan Garip <oguzhan.garip21@gmail.com>
2024-03-17 16:54:21 +0300

    Mount unmounth ornegi yapıldı

---
+++ src/C1.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+function C1({sayac,setSayac}) {
+    
+    useEffect ( ()=> {
+        console.log("c1 render oldu.")
+    }, [sayac])
+    
+    return (
+        <>
+        <div className="bg-success-subtle p-3">
+            Ben C1 komponentiyim.
+            <button onClick={() => {setSayac(eskiDeger => eskiDeger+1)}} className="btn btn-outline-danger">{sayac}</button>
+        </div>
+        </>
+    )
+}
+
+export default C1
